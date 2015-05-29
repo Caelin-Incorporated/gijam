955d88b2b203f24c230b368a382a308bca93f1b2
Lucas Fryzek <nercos7@gmail.com>
2015-05-29 18:50:40 -0400

    add test image

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 var game = new Phaser.Game(1024, 768, Phaser.AUTO, 'GI Jam', {preload: preload, create: create, update: update});
 
 function preload(){
-	
+	game.load.image('guy', "images/test.png");
 }
 
 function create(){
-	
+	game.add.sprite(0,0, 'guy');
 }
 
 function update(){
