b68dcebe16fcb6b1643565ba04905c6635ee3775
Lucas Fryzek <nercos7@gmail.com>
2015-05-29 18:42:09 -0400

    fix up stuff

---
+++ main.js
@@ -0,0 +1,13 @@
+var game = new Phaser.Game(1024, 768, Phaser.AUTO, 'GI Jam', {preload: preload, create: create, update: update});
+
+function preload(){
+	
+}
+
+function create(){
+	
+}
+
+function update(){
+	
+}
