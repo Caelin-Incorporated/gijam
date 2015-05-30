c7cf01a3677294f59c97f64f9a33eb60bd088a31
Caelin <caelink@gmail.com>
2015-05-30 16:44:24 -0400

    Added basic room making logic

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ var buttonLeft, buttonRight;
 var main;
 var hpBar;
 var hpText;
+var talkText;
+var currRoomCost;
 
 var states = {
 	BUILD: 0,
@@ -56,6 +58,15 @@ function create(){
 	bossRoom.sprite = main.create(128, 128, 'room1');
 	dung.rooms.push(bossRoom);
 	
+	currRoomCost = 100;
+	
+	//Making a new room sprite
+	var newRoom = new Room();
+	newRoom.sprite = main.create(128, 128, 'newroom');
+	newRoom.events.onInputUp.add(makeRoom, this);
+	dung.rooms.push(newRoom);
+	
+	
 	// clicky buttons
 	buttonLeft = main.create(64, 360, 'arrow');
 	buttonLeft.anchor.setTo(0.5, 0.5);
@@ -83,6 +94,26 @@ function create(){
 	hpText = game.add.text(10,10,"Health:", {fontSize: '32px', fill:"#FF0000"});
 	main.add(hpText);
 	buttonChal.events.onInputUp.add(toChallenge, this);
+}
+
+function makeRoom (event, sprite) {
+	if(state == state.BUILD) {
+		if (dung.cardboard >= currRoomCost) {
+			dung.rooms.pop();
+			dung.rooms[dung.length] = new Room();
+			dung.rooms[dung.length + 1].sprite = main.create(128, 128, 'room1');
+			dung.cardboard -= currRoomCost;
+			currRoomCost += (currRoomCost / 2);
+		} else {
+			talkText = game.add.text(140, 70,"Sorry, you are " + (currRoomCost - dung.cardboard) + "short!", {fontSize: '32px', fill:"#FF0000"});
+			main.add(talkText);
+		}
+		
+	} else {
+		talkText = game.add.text(140, 70,"Sorry, you can't do that right now", {fontSize: '32px', fill:"#FF0000"});
+		main.add(talkText);
+	}
+	
 }
 
 function toMinion (event, sprite) {
