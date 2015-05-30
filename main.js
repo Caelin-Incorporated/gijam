fc3db0b4dc86eade56343d7f2dadf9ea8066d4d6
Lucas Fryzek <nercos7@gmail.com>
2015-05-29 20:07:05 -0400

    add some basic test images

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var game = new Phaser.Game(1280, 720, Phaser.AUTO, 'GI Jam', {preload: preload, create: create, update: update});
 var dung;
-
+var buttonLeft, buttonRight;
 
 function Dungeon(){
 	this.boss = new Boss();
@@ -8,6 +8,7 @@ function Dungeon(){
 	this.souls = 0;
 	this.rooms = [];
 	this.minions = [];
+	this.currentRoom = 0;
 }
 
 function Boss(){
@@ -27,18 +28,25 @@ function Room(){
 }
 
 function preload(){
-	game.load.image('guy', "images/test.png");
+	game.load.image('room1', "images/room.png");
+	game.load.image('arrow', "images/arrow.png");
 }
 
 function create(){
 	dung = new Dungeon();
 	var bossRoom = new Room();
-	bossRoom.monster = dung.boss();
+	bossRoom.monster = dung.boss;
+	bossRoom.sprite = game.add.sprite(128, 128, 'room1');
 	dung.rooms.push(bossRoom);
-	game.add.sprite(0,0, 'guy');
+	
+	// clicky buttons
+	buttonLeft = game.add.sprite(64, 360, 'arrow');
+	buttonLeft.anchor.setTo(0.5, 0.5);
+	buttonLeft.scale.x = -1;
 	
 }
 
 function update(){
-	
+	for(var i=0; i < dung.rooms.length; ++i){
+	}
 }
